f0a9d9bb0e254b1c4f4d342df79cafb23b54af4d
timpohladthomas <timpohladthomas@gmail.com>
2014-07-15 18:23:24 -0400

    Revert "Revert "settings: Enable location mode header switch""
    This reverts commit 149b1173a16dc5e59e07e883c0f24b5afc7f373a.
    
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -89,6 +89,7 @@ import com.android.settings.inputmethod.InputMethodAndLanguageSettings;
 import com.android.settings.inputmethod.KeyboardLayoutPickerFragment;
 import com.android.settings.inputmethod.SpellCheckersSettings;
 import com.android.settings.inputmethod.UserDictionaryList;
+import com.android.settings.location.LocationEnabler;
 import com.android.settings.location.LocationSettings;
 import com.android.settings.net.MobileDataEnabler;
 import com.android.settings.nfc.AndroidBeam;
@@ -959,6 +960,7 @@ public class Settings extends PreferenceActivity
         private final BluetoothEnabler mBluetoothEnabler;
         private final MobileDataEnabler mMobileDataEnabler;
         public static ThemeEnabler mThemeEnabler;
+        private final LocationEnabler mLocationEnabler;
         private AuthenticatorHelper mAuthHelper;
         private DevicePolicyManager mDevicePolicyManager;
 
@@ -980,7 +982,8 @@ public class Settings extends PreferenceActivity
             } else if (header.id == R.id.wifi_settings
                     || header.id == R.id.bluetooth_settings
                     || header.id == R.id.theme_settings
-                    || header.id == R.id.mobile_network_settings) {
+                    || header.id == R.id.mobile_network_settings
+                    || header.id == R.id.location_settings) {
                 return HEADER_TYPE_SWITCH;
             } else if (header.id == R.id.security_settings) {
                 return HEADER_TYPE_BUTTON;
@@ -1028,6 +1031,7 @@ public class Settings extends PreferenceActivity
             mBluetoothEnabler = new BluetoothEnabler(context, new Switch(context));
             mMobileDataEnabler = new MobileDataEnabler(context, new Switch(context));
             mThemeEnabler = new ThemeEnabler(context, new Switch(context));
+            mLocationEnabler = new LocationEnabler(context, new Switch(context));
             mDevicePolicyManager = dpm;
         }
 
@@ -1103,6 +1107,8 @@ public class Settings extends PreferenceActivity
                         mMobileDataEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.theme_settings) {
                         mThemeEnabler.setSwitch(holder.switch_);
+                    } else if (header.id == R.id.location_settings) {
+                        mLocationEnabler.setSwitch(holder.switch_);
                     }
                     updateCommonHeaderView(header, holder);
                     break;
@@ -1178,6 +1184,7 @@ public class Settings extends PreferenceActivity
             mBluetoothEnabler.resume();
             mMobileDataEnabler.resume();
             mThemeEnabler.resume();
+            mLocationEnabler.resume();
         }
 
         public void pause() {
@@ -1185,6 +1192,7 @@ public class Settings extends PreferenceActivity
             mBluetoothEnabler.pause();
             mMobileDataEnabler.pause();
             mThemeEnabler.resume();
+            mLocationEnabler.pause();
         }
     }
 
